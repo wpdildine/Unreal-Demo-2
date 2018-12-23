e4dc3c071c4e85a2b816d26416bf8c751353c13a
Patrick Dildine <wpdildine@gmail.com>
2018-12-23 15:13:50 -0500

    intended movement

--- TankDemo/Source/TankDemo/Tank.h
+++ TankDemo/Source/TankDemo/Tank.h
@@ -10,6 +10,7 @@ class UTankBarrel;
 class UTankTurret;
 class UTankAimingComponent;
 class AProjectile;
+class UTankMovementComponent;
 
 UCLASS()
 class TANKDEMO_API ATank : public APawn
@@ -31,6 +32,9 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 	UTankAimingComponent * TankAimingComponent = nullptr;
+
+	UPROPERTY(BlueprintReadOnly)
+		UTankMovementComponent * TankMovementComponent = nullptr;
 
 private:	
 	ATank();
